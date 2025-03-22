e187cefeee67c9337190398c967843a45975620c
Mayra <mayra@email.com>
2025-03-22 12:42:01 -0500

    Updated edits lesson

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,24 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = "5" - 2;//number convsersion of string; 
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = Boolean("false");//the if statement executes beacuse the string "false" is a truly value
 if (isValid) {
     console.log("This is valid!");
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5;// + operater triggers string concatenation, converts 5 into string and add it to string variable age"25" to yield string 255
+//added explicit type conversion Number() to variable (age) to change data type from string to number
 console.log("Total Age: " + totalAge);
+
+//added explicit example with edge case of 0
+let compliment = Boolean (totalAge == 0);
+if(compliment){
+  console.log("You are a newborn baby!");
+}
+
+let compliment2 = "You are" + " " + age + " " + "years young!";//added implicit example
+console.log(compliment2);
